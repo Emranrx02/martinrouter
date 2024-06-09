5608b95a19dd76efa1734ffbb9b8b8f21eeac909
Riyad139 <riyad.ahmed1886@gmail.com>
2024-06-09 21:21:19 +0600

    added

--- src/lib/components/Socials/Socials.svelte
+++ src/lib/components/Socials/Socials.svelte
@@ -5,7 +5,7 @@
 <ContentLayout>
 	<div id="socials" class="flex md:flex-row flex-col px-7 gap-10 pt-48">
 		<div class="order-2 md:order-1">
-			<img class="md:h-[40rem] animate-pulse delay-700" src="/router3.png" alt="router 3" />
+			<img class="md:max-h-[40rem] animate-pulse delay-700" src="/router3.png" alt="router 3" />
 		</div>
 		<section class="text-right order-1 md:order-2">
 			<h3 class="text-3xl sm:text-5xl font-medium mb-7">All you can eat is Treat</h3>
