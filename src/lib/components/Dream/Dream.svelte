2267552c061a0ac75b163d939dc23521a4b23625
Riyad139 <riyad.ahmed1886@gmail.com>
2024-07-16 18:54:14 +0600

    added

--- src/lib/components/Dream/Dream.svelte
+++ src/lib/components/Dream/Dream.svelte
@@ -8,13 +8,13 @@
 	<section class="backdrop-brightness-50 min-h-[60rem]">
 		<div id="dream" class="py-40 ml-[5%] flex md:flex-row flex-col">
 			<div class="md:w-[40%] w-full space-y-3">
-				<h2 class=" text-5xl lg:text-7xl font-bold">The $Router of CT</h2>
+				<h2 class=" text-5xl lg:text-7xl font-bold">The $Router of Ton</h2>
 				<h2 class=" text-5xl lg:text-7xl">Martin $Router King</h2>
 				<p class="text-xl">
-					I have a dream of crypto freedom for all <br /> No Router = No connection = No CT
+					I have a dream of crypto freedom for all <br /> No Router = No connection
 				</p>
 			</div>
-			
+
 			<div
 				class="h-[20rem] bg-cover bg-center bg-no-repeat flex items-end justify-center rounded-tl-[30rem] mt-40"
 			></div>
--- src/lib/components/NavBar/NavBar.svelte
+++ src/lib/components/NavBar/NavBar.svelte
@@ -26,13 +26,16 @@
 		if (body && open) {
 			//@ts-ignore
 			body.style = 'overflow: hidden';
-		} else if (body)
+		} else if (body) {
 			//@ts-ignore
 			body.style = 'overflow-y: auto';
+			//@ts-ignore
+			body.style = 'overflow-x: hidden';
+		}
 	}
 </script>
 
-<section class="text-white">
+<section class="text-white w-full overflow-hidden">
 	<ContentLayout>
 		<section class="flex py-5 sm:py-11 sm:px-16 items-center">
 			<img src="/router.png" alt="marting router logo" class=" w-28 lg:w-40 mr-2 mb-11" />
